00168f2f395653c9b51fb74fcfdf7a18918f8353
Michele Ricciardi <mriccia@amazon.com>
2023-06-02 09:24:31 +0200

    Add Tracing and Metrics

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -14,17 +14,28 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
+import software.amazon.cloudwatchlogs.emf.model.DimensionSet;
+import software.amazon.cloudwatchlogs.emf.model.Unit;
 import software.amazon.lambda.powertools.logging.LoggingUtils;
 import software.amazon.lambda.powertools.logging.Logging;
+import software.amazon.lambda.powertools.metrics.Metrics;
+import software.amazon.lambda.powertools.metrics.MetricsUtils;
+import software.amazon.lambda.powertools.tracing.Tracing;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     Logger log = LogManager.getLogger();
+    MetricsLogger metricsLogger = MetricsUtils.metricsLogger();
 
     @Logging(logEvent = true)
+    @Tracing
+    @Metrics(namespace = "java17-sample", service = "helloservice")
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        metricsLogger.putDimensions(DimensionSet.of("environment", "prod"));
+        metricsLogger.putMetric("SuccessfulHello", 1, Unit.COUNT);
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
@@ -32,17 +43,22 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
         try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
-
-            return response
-                    .withStatusCode(200)
-                    .withBody(output);
+            return getApiGatewayProxyResponseEvent(response);
         } catch (IOException e) {
             return response
                     .withBody("{}")
                     .withStatusCode(500);
         }
+    }
+
+    @Tracing(segmentName="yourCustomName")
+    private APIGatewayProxyResponseEvent getApiGatewayProxyResponseEvent(APIGatewayProxyResponseEvent response) throws IOException {
+        final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+        String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+
+        return response
+                .withStatusCode(200)
+                .withBody(output);
     }
 
     private String getPageContents(String address) throws IOException{
